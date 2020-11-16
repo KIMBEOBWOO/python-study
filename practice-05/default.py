ff7f36566fac4070c5819a2c4e4fcd50420c13a3
kimbeobwoo <beob@PC.localdomain>
2020-11-16 22:39:49 +0900

    5 장 미완

---
+++ practice-05/default.py
@@ -0,0 +1,13 @@
+import sys
+import mod1
+import mod2
+
+# print(mod1.add(3, 4))
+# print(mod1.sub(4, 3))
+
+print(mod2.PI)
+a = mod2.Math()
+print(a.solv(2))
+print(mod2.add(mod2.PI, 4.4))
+
+sys.path
---
+++ practice-05/mod1.py
@@ -0,0 +1,11 @@
+# mod1.py
+def add(a, b):
+    return a + b
+
+
+def sub(a, b):
+    return a-b
+
+
+print(add(3, 4))
+print(sub(4, 3))
